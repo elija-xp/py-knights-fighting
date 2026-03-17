6f082271aef5e9b6c5c730854cefd87675ec3878
elija-xp <eyelja@gmail.com>
2026-03-17 17:54:25 +0200

    solution

---
+++ app/warriors/knights.py
@@ -0,0 +1,127 @@
+KNIGHTS = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {
+            "name": "Metal Sword",
+            "power": 50,
+        },
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {
+                "part": "helmet",
+                "protection": 15,
+            },
+            {
+                "part": "breastplate",
+                "protection": 20,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Two-handed Sword",
+            "power": 55,
+        },
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 15,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Poisoned Sword",
+            "power": 60,
+        },
+        "potion": {
+            "name": "Berserk",
+            "effect": {
+                "power": +15,
+                "hp": -5,
+                "protection": +10,
+            }
+        }
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 25,
+            }
+        ],
+        "weapon": {
+            "name": "Sword",
+            "power": 45
+        },
+        "potion": {
+            "name": "Blessing",
+            "effect": {
+                "hp": +10,
+                "power": +5,
+            }
+        }
+    }
+}
+
+
+class Knights:
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            armour: list,
+            weapon: dict,
+            potion: dict
+    ) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        self.prepare()
+
+    def prepare(self) -> None:
+        self.protection = sum(a.get("protection", 0) for a in self.armour)
+
+        # додаємо силу зброї
+        self.power += self.weapon.get("power", 0)
+
+        # додаємо ефект зілля тільки якщо воно є
+        if self.potion:
+            effect = self.potion.get("effect", {})
+            self.power += effect.get("power", 0)
+            self.hp += effect.get("hp", 0)
+            self.protection += effect.get("protection", 0)
+
+    def __repr__(self) -> str:
+        return (f"Knight name - {self.name},\n"
+                f"power - {self.power},\n"
+                f"hp - {self.hp},\n"
+                f"armour - {self.armour},\n"
+                f"weapon - {self.weapon},\n"
+                f"potion - {self.potion}\n"
+                )
